cc02bf569d9ec475304f8c13d5f06074c9c582ad
Himavarshini <yaleruhimavarshini09@gmail.com>
2026-06-11 14:08:43 +0530

    Task 3: H2 database integration - TransactionRecord and balance validation

--- src/main/java/com/jpmc/midascore/component/KafkaTransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/KafkaTransactionListener.java
@@ -1,14 +1,49 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.repository.TransactionRepository;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class KafkaTransactionListener {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TransactionRepository transactionRepository;
+
     @KafkaListener(topics = "${general.kafka-topic}")
     public void listen(Transaction transaction) {
-        System.out.println("Received transaction: " + transaction);
+        Optional<UserRecord> senderOpt = userRepository.findById(transaction.getSenderId());
+        Optional<UserRecord> recipientOpt = userRepository.findById(transaction.getRecipientId());
+
+        if (senderOpt.isPresent() && recipientOpt.isPresent()) {
+            UserRecord sender = senderOpt.get();
+            UserRecord recipient = recipientOpt.get();
+
+            if (sender.getBalance() >= transaction.getAmount()) {
+                sender.setBalance(sender.getBalance() - transaction.getAmount());
+                recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+
+                userRepository.save(sender);
+                userRepository.save(recipient);
+
+                TransactionRecord record = new TransactionRecord(sender, recipient, transaction.getAmount());
+                transactionRepository.save(record);
+                System.out.println("Saved transaction: " + transaction);
+
+                // Print all user balances after each transaction
+                userRepository.findAll().forEach(
+                        user -> System.out.println("USER BALANCE: " + user.getName() + " = " + user.getBalance()));
+            }
+        }
     }
 }
