e400bfc2c40f95f57aba8474391c39c0dc2b47e5
Himavarshini <yaleruhimavarshini09@gmail.com>
2026-06-11 15:21:04 +0530

    Task 4: Incentive API integration - RestTemplate calling incentive endpoint

--- src/main/java/com/jpmc/midascore/component/KafkaTransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/KafkaTransactionListener.java
@@ -2,12 +2,14 @@ package com.jpmc.midascore.component;
 
 import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Incentive;
 import com.jpmc.midascore.foundation.Transaction;
 import com.jpmc.midascore.repository.TransactionRepository;
 import com.jpmc.midascore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
@@ -20,6 +22,8 @@ public class KafkaTransactionListener {
     @Autowired
     private TransactionRepository transactionRepository;
 
+    private final RestTemplate restTemplate = new RestTemplate();
+
     @KafkaListener(topics = "${general.kafka-topic}")
     public void listen(Transaction transaction) {
         Optional<UserRecord> senderOpt = userRepository.findById(transaction.getSenderId());
@@ -30,19 +34,30 @@ public class KafkaTransactionListener {
             UserRecord recipient = recipientOpt.get();
 
             if (sender.getBalance() >= transaction.getAmount()) {
+
+                Incentive incentive = restTemplate.postForObject(
+                        "http://localhost:8081/incentive",
+                        transaction,
+                        Incentive.class);
+                float incentiveAmount = (incentive != null) ? incentive.getAmount() : 0;
+
                 sender.setBalance(sender.getBalance() - transaction.getAmount());
-                recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+                recipient.setBalance(recipient.getBalance() + transaction.getAmount() + incentiveAmount);
 
                 userRepository.save(sender);
                 userRepository.save(recipient);
 
-                TransactionRecord record = new TransactionRecord(sender, recipient, transaction.getAmount());
+                TransactionRecord record = new TransactionRecord(sender, recipient, transaction.getAmount(),
+                        incentiveAmount);
                 transactionRepository.save(record);
-                System.out.println("Saved transaction: " + transaction);
 
-                // Print all user balances after each transaction
-                userRepository.findAll().forEach(
-                        user -> System.out.println("USER BALANCE: " + user.getName() + " = " + user.getBalance()));
+                System.out.println("Saved transaction: " + transaction + " incentive: " + incentiveAmount);
+
+                userRepository.findAll().forEach(user -> {
+                    if (user.getName().equals("wilbur")) {
+                        System.out.println("WILBUR BALANCE: " + user.getBalance());
+                    }
+                });
             }
         }
     }
